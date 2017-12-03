a612456b5d49ef16660080e0227f9cce600bb45b
hoanghedspi <a2k40pbc@gmail.com>
2017-12-03 18:17:40 +0700

    a little bit changed

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/index', function() {
+    return view('index');
 });
 Route::group(['prefix'=>'admin'],function() {
     Route::group(['prefix' => 'person'], function () {
